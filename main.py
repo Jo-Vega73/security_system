3614fb800c5ebe3ebdeb1dc22249537f35595ef9
Jo-Vega73 <jkc2518@icloud.com>
2025-12-14 16:12:51 -0600

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from net_monitor import MonitorRed
 from log_analyzer import AnalizadorLogs
 from vuln_scanner import EscanerVuln
 from reporter import GeneradorReportes
+from web_analyzer import AnalizadorWeb
 
 
 def main():
@@ -16,43 +17,37 @@ def main():
     # --------------------------------------------
     alerter = GestorAlertas()
 
-    #  modo_real = False (seguro para evaluación)
     sistema_prevencion = SistemaPrevencion(
         alerter,
-        modo_real=False
+        modo_real=False   # Cambiar a True solo como admin/root
     )
 
     # --------------------------------------------
     # Módulos
     # --------------------------------------------
     monitor = MonitorRed(sistema_prevencion, alerter)
-    analizador_logs = AnalizadorLogs(alerter)
+    analizador_logs = AnalizadorLogs(alerter, sistema_prevencion)
     scanner = EscanerVuln(alerter)
+    analizador_web = AnalizadorWeb(alerter)
     reporter = GeneradorReportes()
-    
-    # --- EJECUCIÓN DEL FLUJO DE TRABAJO ---
-    
-    # Paso A: Monitoreo de Red
+
+    # --------------------------------------------
+    # Flujo principal
+    # --------------------------------------------
     monitor.iniciar_monitoreo(duracion=15)
-    
-    # Paso B: Análisis de Logs
+
     analizador_logs.analizar()
-    
-    # Paso C: Escaneo de Vulnerabilidades
-    vulns_encontradas = scanner.escanear_sistema("Servidor_Produccion_01")
-    
-    # Paso D: Análisis Web (Prueba interactiva)
+
+    vulns_encontradas = scanner.escanear("Servidor_Produccion_01")
+
     payload = input("\nIngrese una URL para analizar (prueba de inyección): ")
-    analizador_web.analizar_peticion(payload, sistema_prev)
-    
-    # Paso E: Generar Reporte Final
-    ips = sistema_prev.obtener_bloqueos()
+    analizador_web.analizar_peticion(payload, sistema_prevencion)
+
+    ips = sistema_prevencion.obtener_bloqueos()
     reporter.generar_html(ips, vulns_encontradas)
 
-    # Paso F: Resumen final de alertas
     alerter.mostrar_resumen()
 
-    
     print("\n[FIN] Ejecución completada.")
 
 
